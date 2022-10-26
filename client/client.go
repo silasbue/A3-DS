1f70b43f52f6a41e7aa979c04a169bc2ccf7127d
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-10-26 12:34:26 +0200

    implemented message broadcasting

--- client/client.go
+++ client/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"io"
 	"log"
+	"os"
 
 	"github.com/silasbue/A3-DS.git/chitty_chat"
 	"google.golang.org/grpc"
@@ -24,20 +27,26 @@ func main() {
 	go func() {
 		for {
 			in, err := stream.Recv()
-			/*if err == io.EOF {
-				// read done.
+			if err == io.EOF {
 				close(waitc)
 				return
-			}*/
-			// if err != nil {
-			// 	log.Fatalf("Failed to receive a note : %v", err)
-			// }
+			}
 			if err != nil {
 				log.Fatalf("Failed to receive a note : %v", err)
-				break
 			}
 			log.Println("Got message: ", in.Msg)
 		}
 	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		msg := scanner.Text()
+		err := stream.Send(&chitty_chat.Message{Msg: msg})
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	<-waitc
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 
@@ -13,12 +14,33 @@ type Server struct {
 	streams []chitty_chat.ChittyChat_ChatServer
 }
 
-func (s *Server) Chat(stream chitty_chat.ChittyChat_ChatServer) error {
-	s.streams = append(s.streams, stream)
+func (s *Server) connect(newStream chitty_chat.ChittyChat_ChatServer) {
+	s.streams = append(s.streams, newStream)
 	for _, client := range s.streams {
-		client.Send(&chitty_chat.Message{Id: 1, Msg: "A new client has joined", T: 1})
+		client.SendMsg(&chitty_chat.Message{Id: 1, Msg: "A new client has joined", T: 1})
 	}
+}
 
+func (s *Server) Chat(stream chitty_chat.ChittyChat_ChatServer) error {
+	go func() {
+		for {
+			msg, err := stream.Recv()
+
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				return
+			}
+			for _, client := range s.streams {
+				client.SendMsg(&chitty_chat.Message{Msg: msg.GetMsg()})
+			}
+		}
+	}()
+
+	waitc := make(chan struct{})
+	s.connect(stream)
+
+	<-waitc
 	return nil
 }
 
